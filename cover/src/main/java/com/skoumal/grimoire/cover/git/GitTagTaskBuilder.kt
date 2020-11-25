d4bc039e2e182f8952f162487a422174b04f290e
Viktor De Pasquale <info@depasquale.wiki>
2020-11-25 12:50:11 +0100

    Updated git task builder
    Builder now only takes two parameters as mandatory, all other
     parameters are filled or declared on the fly or by default.
     It also now safely creates tasks and extensions in a way both
     aforementioned classes don't overlap. Finally it creates a
     task-set equal in length to the number of supplied variants when
     put into onVariantProperties lambda.

--- cover/src/main/java/com/skoumal/grimoire/cover/git/GitTagTaskBuilder.kt
+++ cover/src/main/java/com/skoumal/grimoire/cover/git/GitTagTaskBuilder.kt
@@ -10,6 +10,9 @@ import org.gradle.api.Project
 import org.gradle.api.file.RegularFile
 import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.api.provider.Provider
+import org.gradle.api.tasks.TaskProvider
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.getByType
 
 /**
  * Builder helps you to create [GitTagTask] hassle free. Make sure to [register] before calling
@@ -25,10 +28,10 @@ import org.gradle.api.provider.Provider
  * }
  * ```
  *
- * @mandatory [setProjectName] or [setTaskName]
  * @mandatory [setApplicationVariant]
  * @mandatory [setIdentity]
  *
+ * @optional [setProjectName] or [setTaskName]
  * @optional [setBuildFile]
  * @optional [setDefaultVersionName]
  * @optional [setDefaultVersionCode]
@@ -49,7 +52,7 @@ class GitTagTaskBuilder(
         .map { it.file("git-version") }
 
     /**
-     * Sets the project name and builds default task name. This parameter is mandatory.
+     * Sets the project name and builds default task name.
      *
      * Example:
      * ```
@@ -64,16 +67,27 @@ class GitTagTaskBuilder(
     fun setProjectName(name: String) = setTaskName("setGitTagFor${name.capitalize()}")
 
     /**
-     * Sets the task name and assigns it to the task created in [build]. This parameter is
-     * mandatory, unless set via [setProjectName]
+     * Sets the task name and assigns it to the task created in [build].
      * */
     fun setTaskName(taskName: String) = apply { this.taskName = taskName }
 
     /**
-     * Sets application variant to be used for output selection. This parameter is mandatory.
+     * Sets application variant to be used for output selection. Setting application variant also
+     * applies [setProjectName] if it was not set previously. Project name is picked so every
+     * variant has exclusive task for its purpose and can have different options and settings.
+     * Keep in mind that [VersionOptions] are, as an extension, set only once and might be invoked
+     * on multiple occasions.
+     *
+     * This parameter is mandatory.
+     *
      * @see [setProjectName] as example
      * */
-    fun setApplicationVariant(variant: ApplicationVariant) = apply { this.variant = variant }
+    fun setApplicationVariant(variant: ApplicationVariant) = apply {
+        this.variant = variant
+        if (!this::taskName.isInitialized) {
+            setProjectName(project.name + variant.name.capitalize())
+        }
+    }
 
     /**
      * Sets build file to whatever file you want and doesn't check the scope or anything.
@@ -110,11 +124,16 @@ class GitTagTaskBuilder(
     fun build() {
         val mainOutput = variant.outputs.single { it.outputType == OutputType.SINGLE }
         val options = options ?: project.extensions.getByType(VersionOptions::class.java)
-        val task = project.tasks.register(taskName, GitTagTask::class.java) {
-            outputFile.set(buildFile)
-            versionOptions.set(options)
-            identity.set(this@GitTagTaskBuilder.identity)
-        }
+        val task = project.tasks.runCatching { named(taskName) as TaskProvider<GitTagTask> }.fold(
+            onSuccess = { it },
+            onFailure = {
+                project.tasks.register(taskName, GitTagTask::class.java) {
+                    outputFile.set(buildFile)
+                    versionOptions.set(options)
+                    identity.set(this@GitTagTaskBuilder.identity)
+                }
+            }
+        )
 
         val defaultName = mainOutput.versionName.getOrElse("1.0.0")
         val defaultCode = mainOutput.versionCode.getOrElse(0)
@@ -122,23 +141,34 @@ class GitTagTaskBuilder(
         val taskOutput = task.map { it.outputFile.runCatching { get().asFile.readLines() } }
 
         mainOutput.versionName.set(
-            taskOutput.map { it.mapCatching { it[0] }.fold({ it }, { defaultName }) }
+            taskOutput.map {
+                it.mapCatching { it[0] }
+                    .fold({ it }, { defaultName })
+                    .also { println("versionName = $it") }
+            }
         )
         mainOutput.versionCode.set(
-            taskOutput.map { it.mapCatching { it[1].toInt() }.fold({ it }, { defaultCode }) }
+            taskOutput.map {
+                it.mapCatching { it[1].toInt() }
+                    .fold({ it }, { defaultCode })
+                    .also { println("versionCode = $it") }
+            }
         )
     }
 
     companion object {
 
         @JvmStatic
-        fun register(container: ExtensionContainer): VersionOptions =
-            container.create("versionUpdater", VersionOptions::class.java)
-
-        @JvmStatic
-        fun VersionOptions.buildUponProject(project: Project) =
-            GitTagTaskBuilder(project, this)
+        fun register(container: ExtensionContainer): VersionOptions {
+            return container.runCatching { getByType<VersionOptions>() }.fold(
+                onSuccess = { it },
+                onFailure = { container.create("versionUpdater") }
+            )
+        }
 
     }
 
 }
+
+fun VersionOptions.buildUponProject(project: Project) =
+    GitTagTaskBuilder(project, this)
