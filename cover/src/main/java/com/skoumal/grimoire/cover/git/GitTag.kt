d78db361fe973e4e712386f12b635f67abd68b29
Viktor De Pasquale <info@depasquale.wiki>
2021-03-24 15:05:05 +0100

    Added explicit version code overrides

--- cover/src/main/java/com/skoumal/grimoire/cover/git/GitTag.kt
+++ cover/src/main/java/com/skoumal/grimoire/cover/git/GitTag.kt
@@ -8,14 +8,27 @@ import org.gradle.api.provider.Provider
 object GitTag {
 
     @JvmOverloads
+    @JvmStatic
     fun into(
+        provider: Provider<GitVersionTag> = GitTagSemanticProvider(),
         variant: ApplicationVariant,
-        provider: Provider<GitVersionTag> = GitTagSemanticProvider()
+        override: VersionCodeOverride
     ) {
         val output = variant.outputs.single { it.outputType == OutputType.SINGLE }
 
         output.versionName.set(provider.map { it.versionName })
-        output.versionCode.set(provider.map { it.versionCode })
+        output.versionCode.set(provider.map { override.onCodeOverride(it.versionCode) })
     }
 
+    @JvmOverloads
+    @JvmStatic
+    fun into(
+        variant: ApplicationVariant,
+        provider: Provider<GitVersionTag> = GitTagSemanticProvider()
+    ) = into(provider, variant) { it }
+
+}
+
+fun interface VersionCodeOverride {
+    fun onCodeOverride(code: Int): Int
 }
